46dc2562a36093a8bc807d27aff4fd6f1f367864
Griffin jones <griffin.jones9191@stu.mtec.edu>
2024-02-07 19:05:40 -0700

    all files

---
+++ Week5Day4Project_1/app.js
@@ -0,0 +1,11 @@
+$(".navItem").each(function(){
+    $(this).css('color', 'white')
+});
+$("#nav").each(function(){
+    $(this).css('background-color', '#145')
+});
+$('li:even').css('background-color', '#ccc');
+$('li:odd').css('background-color', '#aaa');
+$('input').val('griffin')
+
+
